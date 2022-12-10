07a4ef0e9947bd55271f9fe64b008e3568ec7213
dimmerz92 <andrew.weymes@outlook.com>
2022-12-10 18:58:34 +0800

    add comment

--- src/tests/Ship.test.js
+++ src/tests/Ship.test.js
@@ -1,6 +1,7 @@
 import Ship from "../modules/Ship";
 
 describe('hitting a ship until it is sunk', () => {
+    // Verifies that hit and isSunk method works as expected
     const ship = Ship(2);
     test('first hit', () => {
         ship.hit();
