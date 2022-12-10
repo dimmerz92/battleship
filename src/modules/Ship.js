365c8d152e1f40470e083c4349e1df055b2aa17f
dimmerz92 <andrew.weymes@outlook.com>
2022-12-10 20:11:07 +0800

    add size limits to Ship

--- src/modules/Ship.js
+++ src/modules/Ship.js
@@ -1,4 +1,7 @@
 const Ship = (length) => {
+    if (length > 4) {return "Ship length cannot be greater than 4"};
+    if (length < 1) {return "Ship length cannot be less than 1"};
+
     const hit = () => {
         length--;
     };
--- src/tests/Ship.test.js
+++ src/tests/Ship.test.js
@@ -1,5 +1,15 @@
 import Ship from "../modules/Ship";
 
+describe('making ships too big or too small', () => {
+    test('making a ship too big', () => {
+        expect(Ship(5)).toBe("Ship length cannot be greater than 4");
+    });
+
+    test('making a ship too small', () => {
+        expect(Ship(0)).toBe("Ship length cannot be less than 1");
+    });
+});
+
 describe('hitting a ship until it is sunk', () => {
     // Verifies that hit and isSunk method works as expected
     const ship = Ship(2);
